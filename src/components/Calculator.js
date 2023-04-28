19cff35d2039b8497ec9fbd9b7cb03be199281b3
Daniel Falcon <danfall963@gmail.com>
2023-04-27 20:10:06 -0400

    adding more styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,13 +3,13 @@ import { useState } from 'react';
 import calculate from '../logic/calculate';
 
 function Calculator() {
-  /*   const btnValues = [
-      'AC', '+/-', '%', '÷',
-      '7', '8', '9', 'x',
-      '4', '5', '6', '-',
-      '1', '2', '3', '+',
-      '0', '.', '=',
-    ]; */
+  const btnValues = [
+    'AC', '+/-', '%', '÷',
+    '7', '8', '9', 'x',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    '0', '.', '=',
+  ];
 
   const [result, setResult] = useState({
     total: 0,
@@ -35,160 +35,16 @@ function Calculator() {
           </div>
         </div>
         <div className="rows">
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            AC
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            +/-
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            %
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            ÷
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            7
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            8
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            9
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            x
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            4
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            5
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            6
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            -
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            1
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            2
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            3
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            +
-
-          </button>
-        </div>
-        <div className="last-row">
-          <button
-            type="button"
-            className="bigbutton-wrapper"
-            onClick={handleButtonClick}
-          >
-            0
-
-          </button>
-          <button
-            type="button"
-            className="buttons-wrapper"
-            onClick={handleButtonClick}
-          >
-            .
-
-          </button>
-          <button
-            type="button"
-            className="button-equal"
-            onClick={handleButtonClick}
-          >
-            =
-
-          </button>
+          {btnValues.map((value) => (
+            <button
+              type="button"
+              className="buttons-wrapper"
+              onClick={handleButtonClick}
+              key={value}
+            >
+              {value}
+            </button>
+          ))}
         </div>
       </div>
     </div>
